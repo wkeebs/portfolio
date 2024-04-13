c02bc0b277782f8f2d00b08c606992370d8907ca
Will Keeble <will.a.keeble@gmail.com>
2024-04-13 22:38:22 +1000

    Add 'built with'

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Copyright = () => {
   const year = new Date().getFullYear();
-  return <p>&#169; Will Keeble {year}</p>;
+  return <p className="text-lg">&#169; Will Keeble {year}</p>;
 };
 
 export default Copyright;
