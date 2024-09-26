6c4fe5e368502685eb455a275bda18e349d95988
Will Keeble <will.a.keeble@gmail.com>
2024-09-26 16:15:14 +1000

    Update about section styling and project cards

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Glow, GlowCapture } from "@codaworks/react-glow";
 
 interface Project {
   title: string;
@@ -18,15 +17,39 @@ const projectDetails: Project[] = [
     link: "https://devpost.com/software/groupzy",
     image: "src/assets/profile.jpeg",
   },
+  {
+    title: "Groupzy",
+    description:
+      "Competitive study app, built for StartHack 2024. Won first place.",
+    technologies: ["Next.js, Firebase, Vercel"],
+    link: "https://devpost.com/software/groupzy",
+    image: "src/assets/profile.jpeg",
+  },
+  {
+    title: "Groupzy",
+    description:
+      "Competitive study app, built for StartHack 2024. Won first place.",
+    technologies: ["Next.js, Firebase, Vercel"],
+    link: "https://devpost.com/software/groupzy",
+    image: "src/assets/profile.jpeg",
+  },
+  {
+    title: "Groupzy",
+    description:
+      "Competitive study app, built for StartHack 2024. Won first place.",
+    technologies: ["Next.js, Firebase, Vercel"],
+    link: "https://devpost.com/software/groupzy",
+    image: "src/assets/profile.jpeg",
+  },
 ];
 
 const Projects = () => {
   return (
-    <>
+    <div className="flex flex-col gap-8">
       {projectDetails.map((proj: Project) => (
         <ProjectCard proj={proj} />
       ))}
-    </>
+    </div>
   );
 };
 
@@ -38,49 +61,50 @@ const ProjectCard = ({ proj }: ProjectCardProps) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <GlowCapture size={70}>
-      <Glow color="white">
-        <a
-          href={proj.link}
-          className={`max-w-sm block rounded-lg overflow-hidden shadow-lg transform transition-transform duration-500 relative ${
-            isHovered ? "scale-95" : "scale-100"
-          } bg-dark-bg backdrop-filter backdrop-blur-lg text-white hover:cursor-pointer`}
-          target="_blank"
-          rel="noopener noreferrer"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
-          <div className="relative">
-            {/* Optional Image Section */}
-            <img
-              className="w-full h-48 object-cover opacity-80"
-              src={proj.image}
-              alt={proj.title}
-            />
-            <div className="absolute inset-0 bg-gradient-to-b from-transparent to-gray-800 opacity-75"></div>
+    <div className="w-full flex flex-col md:flex-row bg-dark-bg rounded-xl overflow-hidden shadow-2xl transform transition-transform duration-500 border border-accent-light hover:cursor-pointer hover:scale-[0.98]">
+      <a
+        href={proj.link}
+        className="w-full flex flex-col md:flex-row"
+        target="_blank"
+        rel="noopener noreferrer"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        {/* Project Image */}
+        <div className="relative md:w-1/3 w-full">
+          <img
+            className="w-full h-64 md:h-full object-cover opacity-90 transition duration-500 ease-in-out rounded-none"
+            src={proj.image}
+            alt={proj.title}
+          />
+          <div className="absolute inset-0 bg-gradient-to-b from-transparent to-dark-bg opacity-80"></div>
+        </div>
+
+        {/* Project Content */}
+        <div className="w-full h-full flex flex-col justify-between px-6 py-6 md:w-2/3">
+          <div className="flex-grow">
+            <h2 className="text-3xl font-bold text-accent-light mb-3">
+              {proj.title}
+            </h2>
+            <p className="text-gray-300 text-base mb-4">{proj.description}</p>
           </div>
-          <div className="w-full h-full glow:bg-glow/25">
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2 text-white">
-                {proj.title}
-              </div>
-              <p className="text-gray-400 text-base mb-4">{proj.description}</p>
-            </div>
-            <div className="px-6 pt-4 pb-2 flex flex-wrap">
-              {proj.technologies.map((tech, index) => (
-                <span
-                  key={index}
-                  className="inline-block bg-gray-600 text-white rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2 transition duration-300 ease-in-out "
-                >
-                  #{tech}
-                </span>
-              ))}
-            </div>
+
+          {/* Technologies Used */}
+          <div className="flex flex-wrap gap-2">
+            {proj.technologies.map((tech: string, index: number) => (
+              <span
+                key={index}
+                className="inline-block bg-accent-dark text-white rounded-full px-3 py-1 text-sm font-semibold transition duration-300 ease-in-out hover:bg-accent-light"
+              >
+                #{tech}
+              </span>
+            ))}
           </div>
-        </a>
-      </Glow>
-    </GlowCapture>
+        </div>
+      </a>
+    </div>
   );
 };
+
 
 export default Projects;
